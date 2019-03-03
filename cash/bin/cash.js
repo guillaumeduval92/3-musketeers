0e424731edcf65f0b13ae8cb12ff8954413b935d
Guillaume <guitiolord@hotmail.fr>
2019-03-03 22:17:23 +0100

    cash jsdoc & star wars tests

--- cash/bin/cash.js
+++ cash/bin/cash.js
@@ -5,14 +5,22 @@ const money = require('money');
 const chalk = require('chalk');
 const ora = require('ora');
 const currencies = require('../lib/currencies.json');
-
+/**
+*API needed to get the rates of the conversion
+*/
 const {API} = require('./constants');
 
+	/**
+	*Function used to convert the money
+	*/
 const cash = async command => {
 	const {amount} = command;
 	const from = command.from.toUpperCase();
 	const to = command.to.filter(item => item !== from).map(item => item.toUpperCase());
 
+	/**
+	*Displays a message while the money is being converted
+	*/
 	console.log();
 	const loading = ora({
 		text: 'Converting...',
@@ -24,6 +32,11 @@ const cash = async command => {
 	});
 
 	loading.start();
+
+	/**
+	*Use the API to get the rates of the money
+	*Converts it and get the value
+	*/
 
 	await got(API, {
 		json: true
@@ -38,6 +51,11 @@ const cash = async command => {
 				loading.warn(`${chalk.yellow(`The "${item}" currency not found `)}`);
 			}
 		});
+
+	/**
+	*Return the value of the converted money
+	*And catch if there is an error (special message if it's a connection problem)
+	*/
 
 		console.log(chalk.underline.gray(`\nConversion of ${chalk.bold(from)} ${chalk.bold(amount)}`));
 	}).catch(error => {
--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -6,26 +6,27 @@ describe('starwars-names', () => {
     test('should be a fulfilled array', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
+
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(typeof "starWars.all").toBe("string");
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+    	expect(starWars.all).toContain("Luke Skywalker");
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).not.toContain("Ben Quadinaros");
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toContain(starWars.random());
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(starWars.all).toEqual(expect.arrayContaining(starWars.random(Math.floor((Math.random()*10)))));
     });
   });
 });
